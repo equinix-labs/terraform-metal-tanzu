72eb19ebe1ef4f05ef53ac60547fcd6ffdde7cd5
Marques Johansson <mjohansson@equinix.com>
2021-03-20 02:32:54 -0400

    consolidate required_providers to versions.tf
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- variables.tf
+++ variables.tf
@@ -1,31 +1,3 @@
-terraform {
-  required_providers {
-    metal = {
-      source  = "equinix/metal"
-      version = "1.0.0"
-    }
-    local = {
-      source = "hashicorp/local"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    template = {
-      source = "hashicorp/template"
-    }
-    tls = {
-      source = "hashicorp/tls"
-    }
-    nsxt = {
-      source  = "vmware/nsxt"
-      version = "3.1.0"
-    }
-  }
-}
-
 variable "auth_token" {
 }
 
