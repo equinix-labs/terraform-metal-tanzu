3c1055893938a7b6f7eaa380cd4b1df3be77af32
Marques Johansson <mjohansson@equinix.com>
2021-03-20 02:36:27 -0400

    move the required examples/ snippets to the root
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- object-storage.tf
+++ object-storage.tf
@@ -1,7 +1,7 @@
 
 module "distributed-minio" {
-  source  = "equinix/distributed-minio/metal"
-  version = "0.1.0"
+  source     = "equinix/distributed-minio/metal"
+  version    = "0.1.0"
   auth_token = var.auth_token
   project_id = var.project_id
 }
